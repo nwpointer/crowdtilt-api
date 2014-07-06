3d2a271363451ba74ccdeba7c21daf5bc447e19c
Nathan Pointer <nwpointer@gmail.com>
2014-07-05 18:56:11 -0700

    config update

--- config.php
+++ config.php
@@ -4,9 +4,9 @@
 // contact support.api@crowdtilt.com to request these.
 
 $account_details = [
-	'api_url'=>'https://api-sandbox.crowdtilt.com/v1',
-	'api_key'=>'554cac7c0efff25d8784e712b0c274',
-	'secret'=>'70499e57a674ea547868ffcc520564ece919481e'
+	'api_url'=>'',
+	'api_key'=>'',
+	'secret'=>''
 ];
 
 
